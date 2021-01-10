df04a07fd83af821dc72909673ac7784f13ec209
Philip Herron <philip.herron@embecosm.com>
2021-01-10 16:14:26 +0000

    Mark all functions as DECL_PUBLIC such that they can be linked against.
    This will change when we implement visibility but the main method will be
    reserved case that it will always be DECL_PUBLC.

--- gcc/rust/backend/rust-compile-item.h
+++ gcc/rust/backend/rust-compile-item.h
@@ -138,10 +138,13 @@ public:
     // convert to the actual function type
     auto compiled_fn_type = TyTyCompile::compile (ctx->get_backend (), fnType);
 
+    unsigned int flags = 0;
+    flags |= Backend::function_is_visible;
+
+    std::string asm_name = function.function_name;
     Bfunction *fndecl
       = ctx->get_backend ()->function (compiled_fn_type, function.function_name,
-				       "" /* asm_name */, 0 /* flags */,
-				       function.get_locus ());
+				       asm_name, flags, function.get_locus ());
     ctx->insert_function_decl (function.get_mappings ().get_hirid (), fndecl);
 
     // setup the params
